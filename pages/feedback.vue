d5f14a1eee854e87eb747dd8e92b9a96d502857c
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-06-28 21:28:24 +0300

    upd

---
+++ pages/feedback.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col min-h-screen py-80 bg-cover bg-[url(images/feedback/main.jpg)] w-full col-span-full -mt-20 bg-center relative bg-fixed">
+        <div class="wrapper flex flex-col gap-8 md:gap-12 lg:gap-16 z-10 text-[#f9f9f9] items-center">            
+            <p class="text-2xl md:text-3xl lg:text-4xl text-center uppercase">Свяжитесь со мной</p>
+        </div>
+        <div class="absolute bg-[#131313] bg-opacity-60 inset-0"></div>
+    </div>
+    
+</template>
+
+<script setup>
+    useServerSeoMeta({
+		title: 'Свяжитесь со мной',
+		lang: 'ru'
+	})
+
+</script>
