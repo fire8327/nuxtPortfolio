27e10d8b990999f7049acbcea5027e19eb24a773
fire8327 <114702513+fire8327@users.noreply.github.com>
2024-01-25 15:56:20 +0300

    v2.0

--- pages/index.vue
+++ pages/index.vue
@@ -11,9 +11,9 @@
     <div class="flex flex-col items-center text-center gap-6 lg:gap-8 uppercase" id="text">
         <p class="text-2xl md:text-3xl lg:text-4xl">Краткая информация обо мне</p>
         <p class="text-lg text-[#131313]/60 dark:text-[#f9f9f9]/60">Меня зовут Леонид. Я Веб-разработчик. Специализируюсь на фронтенде и использую tailwindcss для вёрстки сайтов. Изучаю vue и nuxt. Надеюсь, вы <span class="text-transparent !bg-clip-text animate-text">горите своим делом</span> также, как я.</p>
-        <div class="grid grid-cols-2 items-center w-full lg:w-1/2">
-            <NuxtImg width="368px" height="510px" sizes="100vw" src="/images/about/1.webp" alt="" class="-rotate-6 w-full"/>
-            <NuxtImg width="368px" height="510px" sizes="100vw" src="/images/about/2.webp" alt="" class="rotate-6 w-full"/>
+        <div class="grid grid-cols-2 items-center w-full md:w-1/2 lg:w-1/3">
+            <NuxtImg width="256px" height="256px" sizes="100vw" src="/images/about/1.webp" alt="" class="-rotate-6 w-full"/>
+            <NuxtImg width="256px" height="256px" sizes="100vw" src="/images/about/2.webp" alt="" class="rotate-6 w-full"/>
         </div>
     </div>
     <CoopOptions/>
